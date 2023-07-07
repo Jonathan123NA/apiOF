4c9aff9faa695829bafbda85d9565cbfe26906e7
OstoaLeonardo <imlost@micorreo.upp.edu.mx>
2023-07-06 20:46:44 -0600

    Agregar cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 const routes = require('./routes');
 
