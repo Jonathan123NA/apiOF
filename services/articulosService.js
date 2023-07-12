bc1dcef25c60f75cb9cb02781a5571dd9ac652fc
JaredPV <jaredpv99@micorreo.upp.edu.mx>
2023-07-12 12:33:59 -0600

    conexion servicio articulos con Azure

--- services/articulosService.js
+++ services/articulosService.js
@@ -1,57 +1,56 @@
 const db = require('../db');
 const connection = db.connection;
 
-function getAllArticles(req, res) {
-    connection.query('SELECT * FROM articulos', (err, rows) => {
-        if (err) 
-            return res.status(500).send(err);
 
-        res.json(rows);
+function getAllArticles(req, res) {
+    db.db.select().from('articulos').then((data) => {
+        return res.status(200).json(data);
+    })
+    .catch((err) => {
+        return res.status(500).json({ message: 'Error al obtener los articulos' });
     });
 }
 
 function getArticleById(req, res) {
     const id = req.params.id;
-    connection.query('SELECT * FROM articulos WHERE id = ?', id, (err, rows) => {
-        if (err) 
-            return res.status(500).send(err);
-
-        if (rows.length === 0) {
-            return res.status(404).send('Articulo no encontrado');
-        }
-
-        res.json(rows[0]);
+    db.db.select().from('articulos').where('id', id).then((data) => {
+        return res.status(200).json(data[0]);
+    })
+    .catch((err) => {
+        return res.status(500).json({ message: 'Error al obtener el articulo' });
     });
 }
 
 function createArticle(req, res) {
     const articulo = req.body;
-    connection.query('INSERT INTO articulos SET ?', articulo, (err, result) => {
-        if (err) 
-            return res.status(500).send(err);
-
-        res.json({ message: 'Articulo añadido!' });
+    db.db.insert(articulo).into('articulos').then((data) => {
+        return res.status(200).json({ message: 'Articulo creado correctamente' });
+    })
+    .catch((err) => {
+        return res.status(500).json({ message: 'Error al crear el articulo' });
     });
 }
 
 function deleteArticle(req, res) {
     const id = req.params.id;
-    connection.query('DELETE FROM articulos WHERE id = ?', id, (err, result) => {
-        if (err) 
-            return res.status(500).send(err);
-
-        res.json({ message: 'Articulo eliminado!' });
+    db.db('articulos').where('id', id).del().then((count) => {
+        if(count == 0) return res.status(404).json({ message: 'Articulo no encontrado' });
+        return res.status(200).json({ message: 'Articulo eliminado correctamente' });
+    })
+    .connection((err) => {
+        return res.status(500).json({ message: 'Error al eliminar el articulo' });
     });
 }
 
 function updateArticle(req, res) {
     const id = req.params.id;
     const articulo = req.body;
-    connection.query('UPDATE articulos SET ? WHERE id = ?', [articulo, id], (err, result) => {
-        if (err) 
-            return res.status(500).send(err);
-
-        res.json({ message: 'Articulo actualizado!' });
+    db.db('articulos').where('id', id).update(articulo).then((count) => {
+        if(count == 0) return res.status(404).json({ message: 'Articulo no encontrado' });
+        return res.status(200).json({ message: 'Articulo actualizado correctamente' });
+    })
+    .connection((err) => {
+        return res.status(500).json({ message: 'Error al actualizar el articulo' });
     });
 }
 
